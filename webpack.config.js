0740ad7fd8622e22b3067d1c4beddf4ded1d0049
MAshraf <ee.mashraf@gmail.com>
2018-11-20 11:02:26 +0200

    footer bottom

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
       use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader','sass-loader']
     },
     {
-      test: /\.(woff(2)?|ttf|eot|otf|svg)(\?v=\d+\.\d+\.\d+)?$/,
+      test: /\.(woff(2)?|ttf|eot|otf)(\?v=\d+\.\d+\.\d+)?$/,
       use: [{
           loader: 'file-loader',
           options: {
