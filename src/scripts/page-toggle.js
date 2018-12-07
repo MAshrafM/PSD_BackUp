19167998c600b5740d3f90cd2abd3e5eaea8f2eb
MAshraf <ee.mashraf@gmail.com>
2018-12-07 15:02:51 +0200

    new page toggle

--- src/scripts/page-toggle.js
+++ src/scripts/page-toggle.js
@@ -1,11 +1,34 @@
 const HomePage = $("#home-page");
 const OverviewPage = $("#overview-page");
+const FeaturesPage = $("#features-page")
 
 const OverviewLink = $("#overview");
+const FeaturesLink = $("#features");
 
 OverviewLink.click((e) => {
   e.preventDefault();
-  HomePage.fadeOut(1000, () => {
-    OverviewPage.fadeIn(1000);
-  });
+  if(HomePage.is(':visible')){
+    HomePage.fadeOut(1000, () => {
+      OverviewPage.fadeIn(1000);
+    });
+  }
+  if(FeaturesPage.is(':visible')){
+    FeaturesPage.fadeOut(1000, () => {
+      OverviewPage.fadeIn(1000);
+    });
+  }
+});
+
+FeaturesLink.click((e) => {
+  e.preventDefault();
+  if(HomePage.is(':visible')){
+    HomePage.fadeOut(1000, () => {
+      FeaturesPage.fadeIn(1000);
+    });
+  }
+  if(OverviewPage.is(':visible')){
+    OverviewPage.fadeOut(1000, () => {
+      FeaturesPage.fadeIn(1000);
+    });
+  }
 })
